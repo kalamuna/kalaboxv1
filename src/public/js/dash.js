4e42db84cd10edc4dd004e7d78943937f3a5195c
Will Hastings <will@kalamuna.com>
2013-05-17 16:11:46 -0700

    Set status display for Kalabox to update automatically.

--- src/public/js/dash.js
+++ src/public/js/dash.js
@@ -23,16 +23,40 @@ var dash = (function($, ko, socket) {
     }
   };
 
+  // Status displays:
+  self.statusDisplays = ko.observableArray();
+  function getDisplayStatus() {
+    if (this.running()) {
+      return 'Running';
+    }
+    return 'Stopped';
+  }
+  function getStopped() {
+    return !this.running();
+  }
+  function addStatusDisplay(display) {
+    display.running = ko.observable(false);
+    display.message = ko.computed({read: getDisplayStatus, owner: display});
+    display.stopped = ko.computed({read: getStopped, owner: display});
+    self.statusDisplays.push(display);
+  }
+  var boxStatusDisplay = {
+    name: 'Kalabox'
+  };
+  addStatusDisplay(boxStatusDisplay);
+
   // Server event handlers.
   socket.on('boxStarted', function(data) {
     boxRunning = true;
     self.powerButton.label('Stop');
     self.powerButton.disabled(false);
+    boxStatusDisplay.running(true);
   });
   socket.on('boxStopped', function(data) {
     boxRunning = false;
     self.powerButton.label('Start');
     self.powerButton.disabled(false);
+    boxStatusDisplay.running(false);
   });
 
   // Return public interface.
