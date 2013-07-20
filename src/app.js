254f7198d925896eeb11e57c2133d899b75dfc45
Alec Reynolds <reynolds.alec@gmail.com>
2013-07-19 18:05:36 -0700

    Add service links to home page, stylize help button as a question mark icon

--- src/app.js
+++ src/app.js
@@ -91,7 +91,7 @@ logger.initialize(function() {
     // create window with url: http://localhost:51686/ instead of http://appjs/
     var window = appjs.createWindow('http://localhost:51686/', {
       width : 700,
-      height : 490,
+      height : 520,
       resizable: false,
       icons : __dirname + '/public/icons'
     });
