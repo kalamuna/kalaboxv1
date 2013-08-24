49a476c671508d1a276d68038439fb7f8a818359
Alec Reynolds <reynolds.alec@gmail.com>
2013-08-23 18:13:29 -0700

    Attempt at adding validation

--- src/public/js/dash/sites.js
+++ src/public/js/dash/sites.js
@@ -138,6 +138,7 @@ var newSiteForm = exports.newSiteForm = {
     // Load form into modal from template.
     modal.template('new-site-form');
     modal.show();
+    newSiteFormValidate();
   }
 };
 newSiteForm.onComplete = newSiteForm.onComplete.bind(newSiteForm);
@@ -245,3 +246,29 @@ socket.on('boxStopped', function(data) {
   builtSites.removeAll();
   unbuiltSites.removeAll();
 });
+
+function newSiteFormValidate() {
+  console.log('THIS RAN 1');
+  var validator = new FormValidator('new-site-form', [{
+    name: 'new-site-id',
+    rules: 'callback_check_site_id',
+  }], function(errors, event) {
+    if (errors.length > 0) {
+      var errorString = '';
+
+      for (var i = 0, errorLength = errors.length; i < errorLength; i++) {
+          errorString += errors[i].message + '<br />';
+      }
+
+      $('<div class="alert alert-danger">' + errorString + '</div>').insertBefore('.modal-body');
+    }
+  });
+  console.log(validator);
+  validator.registerCallback('check_site_id', function(value) {
+    console.log('THIS RAN');
+    var patt = new RegExp('[0-9a-z-]*$');
+    console.log(patt.test(value));
+    return patt.test(value);
+  }).setMessage('check_site_id', 'Please set a site URL that only has lowercase letters, numbers, and dashes.');
+
+}
