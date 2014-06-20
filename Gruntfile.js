534d8dcd5781cfca5756e07534c6c549cdace0fd
Steven Bassett <steven.j.bassett@gmail.com>
2014-06-20 16:53:50 -0700

    feat(developer tools): Grunt tasks, travis, editorconfig and .jshintrc

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = function (grunt) {
+  // Show elapsed time at the end
+  require('time-grunt')(grunt);
+  // Load all grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      app: {
+        src: ['lib/**/*.js', 'kalabox/**/*.js', 'scripts/**/*.js', './*.js', '!Gruntfile.js']
+      },
+      specs: {
+        src: ['test/**/*.js']
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      app: {
+        files: '<%= jshint.app.src %>',
+        tasks: ['jshint:lib']
+      },
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('default', ['jshint']);
+
+};
